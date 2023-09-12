69a93bbcbe552f00ce7b8021f165bbb27baa9c74
therealPatrick <malamashangala13@gmail.com>
2023-09-12 19:55:02 +0200

    update password

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,20 @@
+class PasswordsController < ApplicationController::Base
+    before_action :require_user_logged_in!
+
+    def edit
+    end
+
+    def update
+        if Current.user.update(password_params)
+            redirect_to root_path, notice: "Password Upadated"
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
+    end
+end
