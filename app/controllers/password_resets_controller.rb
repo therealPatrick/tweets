74498dcfef9acfc37da1394ff8e58cc4c816fdab
therealPatrick <malamashangala13@gmail.com>
2023-09-13 19:28:13 +0200

    bug fix

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,7 +5,7 @@ class PasswordResetsController < ApplicationController
     def create
         @user = User.find_by(email: params[:email])
         if @user.present?
-            PasswordMailer.with(user: @user).reset.deliver_later
+            PasswordMailer.with(user: @user).reset.deliver_now
     end
     redirect_to root_path, notice: "If an account with this email was found, we have sent a reset a link to your email"
 end
