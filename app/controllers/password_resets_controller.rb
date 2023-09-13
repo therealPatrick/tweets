3caa535ef4c14355cea5c39e54674c7316cfb780
therealPatrick <malamashangala13@gmail.com>
2023-09-13 19:27:47 +0200

    initial

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,6 +5,7 @@ class PasswordResetsController < ApplicationController
     def create
         @user = User.find_by(email: params[:email])
         if @user.present?
+            PasswordMailer.with(user: @user).reset.deliver_later
     end
     redirect_to root_path, notice: "If an account with this email was found, we have sent a reset a link to your email"
 end
